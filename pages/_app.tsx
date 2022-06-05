aef686975a846f50e27a008c98d07a0c6849ce39
Ortovoxx <56805259+Ortovoxx@users.noreply.github.com>
2022-06-05 22:08:05 +0100

    Add sessions to root app component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import Head from "next/head";
+import {SessionProvider} from "next-auth/react"
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps: {session, ...pageProps}}: AppProps) {
+    return (
+        <>
+            <Head>
+                <title>Whatsapp chat analysis</title>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
+            <SessionProvider session={session}>
+                <Component {...pageProps} />
+            </SessionProvider>
+        </>
+    )
 }
 
 export default MyApp
